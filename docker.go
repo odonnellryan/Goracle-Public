110881c8f4de5de4575b2621b32af567bf586165
Ryan O'Donnell <odonnellryanc@gmail.com>
2013-11-15 13:08:21 -0500

    Fixed structs (compiles)

--- docker.go
+++ docker.go
@@ -2,84 +2,84 @@
 package main
 
 import (
-	//"fmt"
+//"fmt"
 )
 
 // RO: added some structs and stuff to properly
 // communicate with Docker. Dunno if this is the
-// best way to do this. following: 
+// best way to do this. following:
 // http://docs.docker.io/en/latest/api/docker_remote_api_v1.6/
 
 type ContainerInfo struct {
-	Id 				string
-    Image 			string
-    Command 		string
-    Created 		int
-    Status 			string
-	Ports 			map[string]string
-	SizeRw 			int
-	SizeRootFs 		int
+	Id         string
+	Image      string
+	Command    string
+	Created    int
+	Status     string
+	Ports      map[string]string
+	SizeRw     int
+	SizeRootFs int
 }
 
 type CreateContainer struct {
-	Hostname 		string
-	User			string
-	Memory			int
-	MemorySwap 		int
-	AttachStdin		bool
-	AttachStout		bool
-	AttachStderr	bool
-	PortSpecs		string
-	Privileged		bool
-	Tty				bool
-	OpenStdin		bool
-	StdinOnce		bool
-	Env				string
-	Cmd				string
-	Dns				string
-	Image			string
-	Volumes			string
-	VolumesFrom		string
-	WorkingDir		string
+	Hostname     string
+	User         string
+	Memory       int
+	MemorySwap   int
+	AttachStdin  bool
+	AttachStout  bool
+	AttachStderr bool
+	PortSpecs    string
+	Privileged   bool
+	Tty          bool
+	OpenStdin    bool
+	StdinOnce    bool
+	Env          string
+	Cmd          string
+	Dns          string
+	Image        string
+	Volumes      string
+	VolumesFrom  string
+	WorkingDir   string
 }
 
 type ExportContainer struct {
-	Content-Type	string
-	STREAM			byte
+	ContentType string
+	STREAM      byte
 }
 
 type StartContainer struct {
-	Binds			string
+	Binds string
 	// docker example: {"lxc.utsname":"docker"}
-	LxcConf			map[string]string
+	LxcConf map[string]string
 }
 
 type StopContainer struct {
-	Id				string
+	Id string
 }
 
 type RestartContainer struct {
-	Id				string
+	Id string
 }
 
 type KillContainer struct {
-	Id				string
+	Id string
 }
 
 type ListContainers struct {
 	ContainerInfoList ContainerInfo
 }
 
-type CreateImageFromChanges {
-	Container		string
-	Repo			string
-	Tag				string
-	M 				string
-	Author			string
+type CreateImageFromChanges struct {
+	Container       string
+	Repo            string
+	Tag             string
+	M               string
+	Author          string
 	ContainerParams CreateContainer
 }
 
-type SearchImages {
-	SearchTerm		string
-	Results			string
+type SearchImages struct {
+	SearchTerm string
+	Results    string
 }
