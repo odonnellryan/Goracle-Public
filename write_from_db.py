25d63cd3eb0051c7af093fef1048449bc22bddd6
Ryan ODonnell <odonnellryanc@gmail.com>
2013-11-29 11:17:49 -0500

    fixed conf write loc. writes when file is modified

--- write_from_db.py
+++ write_from_db.py
@@ -12,27 +12,31 @@ except NameError:
 #
 # TODO: please, please fix the fact that this can break if a config breaks but it isn't one of the fiels in the
 # below paths.
-# 
-# ALSO: it only currently runs the write_check_null thing when a new file is added or after it's modified. it's running it too late
+#
 
 file_path = "nginx/"
 
-def set_write_check_null(cursor, config_id, _file):
-    conn = MySQLdb.connect(host = '127.0.0.1', user = 'root', passwd = '0okm9ijn', db = 'nginx')
-    cursor = conn.cursor()
-    search_file = _file.split("/")
-    e_file = _file.split(".")
-    print e_file
-    n_file = ".".join((e_file[0],"ERROR"))
-    db_write_file = n_file.split("/")
-    print "Renaming file to: " + n_file
-    os.rename(_file, n_file)
-    print "searching in db for: " + search_file[1]
-    cursor.execute("UPDATE configs SET filename = %s, write_check = 0000000000 WHERE filename = %s", (db_write_file[1], search_file[1]))
-    conn.commit()
-    conn.close()
+def set_write_check_null(cursor, _file):
+    try:
+        conn = MySQLdb.connect(host = '127.0.0.1', user = 'root', passwd = '0okm9ijn', db = 'nginx')
+        cursor = conn.cursor()
+        search_file = _file.split("/")
+        e_file = _file.split(".")
+        n_file = ".".join((e_file[0],"ERROR"))
+        db_write_file = n_file.split("/")
+        print "Renaming file to: " + n_file
+        os.rename(_file, n_file)
+        print "Searching in db for: " + search_file[1]
+        cursor.execute("UPDATE configs SET filename = %s, write_check = 0000000000 WHERE filename = %s", (db_write_file[1], search_file[1]))
+        if cursor.rowcount:
+            print "Successfully updated DB"
+        conn.commit()
+        conn.close()
+    except OperationalError:
+        print "Database Down"
+        return False
 
-def write_file_(_file, content, file_hash, write_check, cursor, config_id):
+def write_file_(_file, content, file_hash, write_check, cursor):
     with open(_file, "w") as f:
         if write_check:
             content = "".join(["#", file_hash, "\n", content])
@@ -47,23 +51,19 @@ def write_file_(_file, content, file_hash, write_check, cursor, config_id):
                            stderr=subprocess.PIPE)
                 out, err = process.communicate()
                 errcode = process.returncode
-        comm = out + err
+                if err:
+                    # major error if we can't reload...no configs will reload...
+                    print "Major error - nginx configs not reloading. Error is: " + err
+                    print "Attempting to set config to incorrect..."
+                    e_file = err.split("/")
+                    if (e_file[-2] + "/") == file_path:
+                        error_file = ("".join((file_path,e_file[-1]))).split(":")
+                        error_file = error_file[0]
+                        print "File with error:" + error_file
+                        set_write_check_null(cursor, error_file)
+                        return False
             except (ImportError, ReferenceError, WindowsError):
-                comm = None
                 pass
-            if err:
-                # error_file = comm.split("/")[-1]
-                # major error if we can't reload...no configs will reload...
-                print "Major error - nginx configs not reloading. Error is: " + err
-                print "Attempting to set config to incorrect..."
-                e_file = err.split("/")
-                error_file = ("".join((file_path,e_file[-1]))).split(":")
-                error_file = error_file[0]
-                print "File with error:" + error_file
-                set_write_check_null(cursor, config_id, error_file)
-                return False
-        else:
-            pass
     return True
 
 
@@ -85,10 +85,11 @@ def search_and_write():
                     if hash_line == f.readline():
                         pass
                     else:
-                        write_file_(_file,content,file_hash,write_check,cursor,_file)
+                        write_file_(_file,content,file_hash,write_check,cursor)
             except IOError:
-                        write_file_(_file,content,file_hash,write_check,cursor,_file)
+                        write_file_(_file,content,file_hash,write_check,cursor)
             time.sleep(.01)
+        conn.commit()
         conn.close()
         time.sleep(1)
     except OperationalError, e:
